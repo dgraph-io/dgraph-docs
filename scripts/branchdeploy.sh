53172b7e354f70394aad53bf3cced63198d35c1f
Raphael <rderbier@gmail.com>
2023-02-02 09:24:53 -0800

    updat ehugo version and netlify config (#441)
    handle main as a branch deploy in netlify, update hugo version

--- scripts/branchdeploy.sh
+++ scripts/branchdeploy.sh
@@ -16,7 +16,12 @@ RESET='\033[0m'
 # argument $2 is the $HEAD netlify variable which is the branch name
 
 echo "branchdeploy URL $1 branch $2"
-releaseVersion=$(echo $2 | sed 's/.*\///')
+if [[ $2 == "main" ]]; then
+		releaseVersion="main"
+else
+		releaseVersion=$(echo $2 | sed 's/.*\///')
+fi
+
 echo "version '$releaseVersion'"
 VERSION_STRING="$releaseVersion"
 # In Unix environments, env variables should also be exported to be seen by Hugo
