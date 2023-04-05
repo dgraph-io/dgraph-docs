bfdf7021a1eb2ec19004f321978cbe9da0c24bd4
Michel Diz <MichelDiz@users.noreply.github.com>
2023-04-05 00:01:20 -0300

    test

--- scripts/checkgitdiff.sh
+++ scripts/checkgitdiff.sh
@@ -3,6 +3,9 @@
 # obter o nome da branch de origem
 PR_BRANCH=$(echo $GITHUB_REF | sed 's/refs\/pull\///;s/\/head//')
 
+echo $PR_BRANCH
+echo '---'
+echo $PR_HASH
 # obter o hash da commit de destino
 PR_HASH=$(gh pr view $PR_NUMBER --json headRefOid --jq .headRefOid)
 
